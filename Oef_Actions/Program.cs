b6cd993504eea0c131d62265fa5659e1f5861e9a
gillesy <r0793300@student.thomasmore.be>
2021-10-22 14:54:06 +0200

    Fout eruit

--- Oef_Actions/Program.cs
+++ Oef_Actions/Program.cs
@@ -6,7 +6,7 @@ namespace Oef_Actions
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!")
+            Console.WriteLine("Hello World!");
         }
     }
 }
